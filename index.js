91e5430fb5d9e9c737c40d776c718ff6af402d99
meksoftwaredev <146220688+meksoftwaredev@users.noreply.github.com>
2023-11-19 19:51:37 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function odevgit() {
+    let url = "file:///C:/Users/mekso/Desktop/LOBS/UYGULAMA/odevler.html";
+    window.location.href = url;
+}
+
+function duyurugit() {
+    let url = "file:///C:/Users/mekso/Desktop/LOBS/UYGULAMA/duyurular.html";
+    window.location.href = url;
+}
+
+function degerlendirgit() {
+    let url = "file:///C:/Users/mekso/Desktop/LOBS/UYGULAMA/degerlendir.html";
+    window.location.href = url;
+}
+
+function ekogelergit() {
+    let url = "file:///C:/Users/mekso/Desktop/LOBS/UYGULAMA/dahafazla.html";
+    window.location.href = url;
+}
+
+function anasayfadon() {
+    let url = "file:///C:/Users/mekso/Desktop/LOBS/UYGULAMA/index.html";
+    window.location.href = url;
+}
