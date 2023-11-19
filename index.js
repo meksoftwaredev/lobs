1d9cd3780105c42a3dd7d5c42eb5b2252179730b
meksoftwaredev <146220688+meksoftwaredev@users.noreply.github.com>
2023-11-19 20:08:14 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function odevgit() {
+    let url = "odevler.html";
+    window.location.href = url;
+}
+
+function duyurugit() {
+    let url = "duyurular.html";
+    window.location.href = url;
+}
+
+function degerlendirgit() {
+    let url = "degerlendir.html";
+    window.location.href = url;
+}
+
+function ekogelergit() {
+    let url = "dahafazla.html";
+    window.location.href = url;
+}
+
+function anasayfadon() {
+    let url = "index.html";
+    window.location.href = url;
+}
