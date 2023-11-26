a922161e4b010332cb8aaea217a06ca551e21846
meksoftwaredev <146220688+meksoftwaredev@users.noreply.github.com>
2023-11-26 23:48:07 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function anasayfablokgit() {
+    document.querySelector('#anasayfablok').scrollIntoView();
+}
+
+function odevlerblokgit() {
+    document.querySelector('#odevlerblok').scrollIntoView();
+}
+
+function duyurularblokgit() {
+    document.querySelector('#duyurularblok').scrollIntoView();
+}
+
+function ekblokgit() {
+    document.querySelector('#ekblok').scrollIntoView();
+}
