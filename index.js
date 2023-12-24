012a17cb0a6a5722b29142003258abf8deccf219
meksoftwaredev <146220688+meksoftwaredev@users.noreply.github.com>
2023-12-24 19:37:06 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+function varsayilanekran() {
+    document.querySelector('#varsayilanekran').scrollIntoView();
+}
+
+function odevlerekran() {
+    document.querySelector('#odevlerekran').scrollIntoView();
+}
+
+function duyurularekran() {
+    document.querySelector('#duyurularekran').scrollIntoView();
+}
+
+function ekekran() {
+    document.querySelector('#ekekran').scrollIntoView();
+}
+
+function degerlendirekran() {
+    document.getElementById("degerlendirekran").style.left = "0";
+}
+
+function sinavnotlariekran() {
+    document.getElementById("sinavnotlariekran").style.left = "0";
+}
+
+function cik() {
+    document.getElementById("degerlendirekran").style.left = "-100%";
+    document.getElementById("sinavnotlariekran").style.left = "-100%";
+}
